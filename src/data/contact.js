4fc861c0155a096babdbd8520a8c766dcb43a9f9
kailiang-zhong <lemon.zkl@gmail.com>
2023-08-13 22:12:32 +0800

    update contact.js

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,6 +1,8 @@
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 import { faZhihu } from '@fortawesome/free-brands-svg-icons/faZhihu';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
+// import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
 
@@ -14,6 +16,11 @@ const data = [
     link: 'mailto:brice.zhong@gmail.com',
     label: 'Email',
     icon: faEnvelope,
+  },
+  {
+    link: 'https://www.linkedin.com/in/kailiang-zhong-5426a827b',
+    label: 'LinkedIn',
+    icon: faLinkedinIn,
   },
   {
     link: 'https://www.zhihu.com/people/zhong-xian-sheng-4-44/posts',
